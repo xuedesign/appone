4e97d83cd9f59078bfe08aa8bfa03097211a1bf0
xuedesign <elmo@xuedesign.tw>
2013-09-08 23:19:37 +0800

    APP應用展示 完成 #20130908

--- scripts/web_set.js
+++ scripts/web_set.js
@@ -15,6 +15,12 @@ window.onload = externalLinks;
 $(function() {
           $("img").lazyload({placeholder : "../images/grey.gif",effect: "fadeIn"});
       });
+	  
+//跳頁選單
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}  
 	 
 	  
 
